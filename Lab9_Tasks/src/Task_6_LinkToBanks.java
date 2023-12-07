132c773507794a7383c1ec61d393dc46b94a07b3
hitstrelok <hitstrelok@gmail.com>
2023-12-07 18:11:19 +0100

    Fixed Task6 (Banks)

--- Lab9_Tasks/src/Task_6_LinkToBanks.java
+++ Lab9_Tasks/src/Task_6_LinkToBanks.java
@@ -1,33 +1,40 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Task_6_LinkToBanks {
     public static void main(String[] args) {
         String link = "https://ewib.nbp.pl/plewibnra?dokNazwa=plewibnra.txt";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 3 first digits from your bank id:");
+        String number = scanner.nextLine();
+
         try {
             URL url = new URL(link);
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            FileWriter fw = new FileWriter("bankData.txt");
 
+            boolean flag=false;
             String line = br.readLine();
             while(br.readLine() != null){
                     String[] array =br.readLine().split("\\t+");
                     if(array.length>2){
-                        String id = array[0].trim();
+                        String id = array[0].trim().substring(0,3);
                         String bankName = array[1].trim();
-                        fw.write(id + "\s" + bankName + "\n");
+                        if(Objects.equals(number, id)){
+                            System.out.println(id + "\s" + bankName + "\n");
+                            flag=true;
+                            break;
+                        }
                     }
             }
-
+            if(flag==false){
+                System.out.println("Bank not found");
+            }
             br.close();
-            fw.close();
-            System.out.println("Copied!");
         }
         catch (Exception e){
             System.out.println(e);
