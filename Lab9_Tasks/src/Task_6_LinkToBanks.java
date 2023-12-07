ab11c6eb7257d4248c77e118d67b333c37b2879a
hitstrelok <hitstrelok@gmail.com>
2023-12-07 18:18:45 +0100

    Added try with resources

--- Lab9_Tasks/src/Task_6_LinkToBanks.java
+++ Lab9_Tasks/src/Task_6_LinkToBanks.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Task_6_LinkToBanks {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         String link = "https://ewib.nbp.pl/plewibnra?dokNazwa=plewibnra.txt";
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 3 first digits from your bank id:");
         String number = scanner.nextLine();
+        URL url = new URL(link);
 
-        try {
-            URL url = new URL(link);
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
             boolean flag=false;
             String line = br.readLine();
             while(br.readLine() != null){
@@ -34,7 +33,6 @@ public class Task_6_LinkToBanks {
             if(flag==false){
                 System.out.println("Bank not found");
             }
-            br.close();
         }
         catch (Exception e){
             System.out.println(e);
